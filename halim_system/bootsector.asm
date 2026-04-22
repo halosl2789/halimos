1d511b74103f9e7a2104e486a0d93ff38b54d59e
halosl2789 <138588891+halosl2789@users.noreply.github.com>
2026-04-22 02:29:57 +0000

    Update bootsector.asm

--- halim_system/bootsector.asm
+++ halim_system/bootsector.asm
@@ -6,17 +6,17 @@ start:
     mov ds, ax
     mov es, ax
     
-    ; تحميل النظام (system.bin) إلى العنوان 0x8000
+    
     mov ah, 0x02
-    mov al, 10          ; تحميل 20 قطاع
+    mov al, 10          
     mov ch, 0
     mov dh, 0
-    mov cl, 2           ; نبدأ من القطاع الثاني
+    mov cl, 2           
     mov bx, 0x8000
     int 0x13
     
     jc disk_error
-    jmp 0x8000          ; القفز إلى كود الـ 32-بت
+    jmp 0x8000          
 
 disk_error:
     mov ah, 0x0E
@@ -25,4 +25,4 @@ disk_error:
     hlt
 
 times 510-($-$$) db 0
-dw 0xAA55               ; التوقيع السحري
+dw 0xAA55               
